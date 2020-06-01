be3be4c7878372ec390a3d34105ca2c41184b784
McTavish420 <abirmctavish@gmail.com>
2020-05-31 23:23:37 -0800

    create all the modules for all the tables in database

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap')
-  const port = 3000
+  const port = 3001
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   logger.log(`Application has started listening to port ${port}`)
